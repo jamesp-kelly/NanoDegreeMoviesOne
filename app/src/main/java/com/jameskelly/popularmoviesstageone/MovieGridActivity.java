51c59ef951628e986ef083b391bc4c5158425635
James Kelly <jk_jameskelly@outlook.com>
2016-05-20 10:23:55 -0400

    initial commit

---
+++ app/src/main/java/com/jameskelly/popularmoviesstageone/MovieGridActivity.java
@@ -0,0 +1,12 @@
+package com.jameskelly.popularmoviesstageone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieGridActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_movie_grid);
+  }
+}
